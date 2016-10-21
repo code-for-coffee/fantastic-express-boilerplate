08b70dede0b497ce2178a73fb1c62ae396cfa313
code-for-coffee <jtraverjr@gmail.com>
2016-10-20 22:26:49 -0500

    :squirrel: hiding the db away until next spring

---
+++ models/db.js
@@ -0,0 +1,23 @@
+'use strict'
+
+require('dotenv').config();
+
+/*
+ Create and setup a database user first!
+ mysql> create database fantastic;
+ # create user identified by password
+ mysql> create user 'l33tdba'@'localhost' identified by 'w0rk5pac3';
+ #give user to access to database.tablename or database.* (all)
+ mysql> grant all privileges on fantastic.* to 'l33tdba'@'localhost';
+ */
+const db = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USR,
+    password : process.env.DB_PW,
+    database: process.env.DB_NAME
+  }
+});
+
+module.exports = db;
