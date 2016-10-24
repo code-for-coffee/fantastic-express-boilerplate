d7d00e2e4b446eda5ae54a6c0ff1150e5a9b84f9
code-for-coffee <jtraverjr@gmail.com>
2016-10-23 21:10:19 -0500

    get *, get :id, post

--- controllers/roles.js
+++ controllers/roles.js
@@ -1,56 +1,89 @@
 const express = require('express');
 let ctrl = express.Router();
-let RoleModel = require('./models/Role');
+let RoleModel = require('../models/Role');
 
-/* GET rolesCtrl listing. */
+/* mimic constrains set in SQL databases for req objects */
+function validateRequest(req) {
+  let name = req.body['name'],
+      body = req.body['body'];
+  if (name.length <= 80
+    && name.length > 0
+    && description.length <= 255) return true;
+  return false;
+}
+
+/* GET * listing => [] */
 ctrl.get('/', (req, res, next) => {
   RoleModel.collection().fetch().then((models) => {
-    res.json(models);
+     res.json(buildResponse(models));
   })
 });
 
-// ctrl.get('/:id', (req, res, next) => {
-//   RoleModel.where({ id: req.params.id })
-//     .fetch()
-//     .then((model) => {
-//       res.json(model);
-//   })
-// });
-
-ctrl.post('/', (req, res, next) => {
-  console.log(req.body);
-  res.send('nyi');
+/* GET by id => {} */
+ctrl.get('/:id', (req, res, next) => {
+  RoleModel.where({ id: req.params.id })
+    .fetch()
+    .then((model) => {
+      res.json(buildResponse(model));
+  })
 });
 
+/* POST => {} */
+ctrl.post('/', (req, res, next) => {
+  if(validateRequest(req)) {
+    let model = new RoleModel({
+      name: 'user',
+      description: 'Plebians'
+    })
+    .save()
+    .then(function (model) {
+      res.json(buildResponse(model));
+    });
+  } else {
+    res.json(
+      sendInvalidResponseJSON('Request Body missing required properties.')
+    );
+  }
+});
+
+/* PUT by id => {} */
 ctrl.put('/:id', (req, res, next) => {
   console.log(req.params.id);
   console.log(req.body);
   res.send('nyi');
 });
 
+/* PATCH by id => {} */
 ctrl.patch('/:id', (req, res, next) => {
   console.log(req.params.id);
   console.log(req.body);
   res.send('nyi');
 });
 
+/* DELETE by id => {} */
 ctrl.delete('/:id', (req, res, next) => {
   console.log(req.params.id);
   console.log(req.body);
   res.send('nyi');
 });
 
-ctrl.get('/create', (req, res, next) => {
-  console.log('cud')
-  let model = new RoleModel({
-    name: 'user',
-    description: 'Plebians'
-  })
-  .save()
-  .then(function (model) {
-    res.json(model);
-  });
-});
+/* private functions */
+function buildResponse(model) {
+  if (!model) {
+    return sendInvalidResponseJSON('No models exist in database matching your request.')
+  } else {
+    return model;
+  }
+}
+
+function sendInvalidResponseJSON(msg) {
+  return {
+    'status': 400,
+    data: {
+      'message': msg
+    }
+  }
+}
 
 module.exports = ctrl;
 
